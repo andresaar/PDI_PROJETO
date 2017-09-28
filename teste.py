0c737f76e3d4e35f6fd8ca03737894a5dfe67d6e
Andres <andresaar94@gmail.com>
2017-09-28 15:16:54 -0300

    mido

--- teste.py
+++ teste.py
@@ -1,36 +1,12 @@
-import sc, time, os
+import mido
+from time import sleep
 
 
-sc.start(exedir="C:\Program Files\SuperCollider-3.8.0", verbose=1)
+names = mido.get_output_names()
+print(names)
+output = mido.open_output(names[0])
 
-time.sleep(3) # wait while sound plays
-
-s = sc.sc.server ###################
-
-s.sendMsg('/d_load', os.path.join(os.getcwd(),'synthdefs','StereoPlayer.scsyndef'));
-
-time.sleep(1)
-
-# no data at buffer index 500 -- silent synth
-s.sendMsg("/s_new", "StereoPlayer", 1967)
-
-time.sleep(1)
-
-s.sendMsg('/b_allocRead', 1000, os.path.join(os.getcwd(),'sounds','numeros.wav'), 0, -1);
-
-time.sleep(1)
-
-# but here, of course
-s.sendMsg("/n_set", 1967, "bufnum", 1000);
-s.sendMsg("/n_set", 1967, "pos", 0.1);
-
-
-s.sendMsg('/d_load', os.path.join(os.getcwd(),'synthdefs','sine.scsyndef'));
-
-time.sleep(1)
-
-s.sendMsg("/s_new", "sine", 1968);
-
-time.sleep(3)
-
-s.sendMsg("/quit")
+for i in range(60,70):
+    output.send(mido.Message('note_on', note=i, velocity=64))
+    sleep(1)
+    output.send(mido.Message('note_off', note=i, velocity=0))
